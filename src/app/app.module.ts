177ffc0db72d4f3569014723f2a1acdfae9c293d
unknown <sabrow@hotmail.fr>
2023-05-21 12:34:58 +0200

    add api service test, component navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './public/navbar/navbar.component';
+import { ArticlesService } from './protected/services/articles/articles.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    ArticlesService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/public/blog/articles-list/articles-list.component.ts
+++ src/app/public/blog/articles-list/articles-list.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-articles-list',
   templateUrl: './articles-list.component.html',
   styleUrls: ['./articles-list.component.scss']
 })
-export class ArticlesListComponent {
-
+export class ArticlesListComponent implements OnInit{
+  articlesList: any;
+  
+  constructor(private http: HttpClient) { }
+  
+  ngOnInit():void {
+    this.http.get(`https://jsonplaceholder.typicode.com/todos`)
+    .subscribe(response => {
+      console.table(response);
+      this.articlesList = response;
+    }
+  );
+  
+   }
+  selectArticle(articleName: string) {
+    console.log(`vous avez cliqué sur ${articleName}`);
+  }
 }

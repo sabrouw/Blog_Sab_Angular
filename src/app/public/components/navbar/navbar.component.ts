a1e7224bfd1a8cedc7d60218c37a7121440cd5aa
Sabrina B <sabrow@hotmail.fr>
2023-08-04 20:54:42 +0200

    feat: responsive

--- src/app/public/components/navbar/navbar.component.ts
+++ src/app/public/components/navbar/navbar.component.ts
@@ -5,23 +5,29 @@ import {FormControl} from '@angular/forms';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ["./navbar.component.scss"],
-
-
-
 })
+
+  
 export class NavbarComponent {
-  showFiller = false;
+  showFiller: boolean = false;
   disabled = new FormControl(false);
   iconOpen: boolean = false;
   iconClose: string = '../../../../assets/images/close-door-icon.svg';
   iconInitial: string = this.iconClose;
+  isSidebarOpen: boolean = false;
 
   changeIcon() {
-  this.iconOpen = !this.iconOpen;
-  if (this.iconOpen) {
-    this.iconClose = '../../../../assets/images/open-door-icon.svg ';
-  } else {
-    this.iconClose = this.iconInitial;
+    this.iconOpen = !this.iconOpen;
+    if (this.iconOpen) {
+      this.iconClose = '/assets/images/open-door-icon.svg ';
+    } else {
+      this.iconClose = this.iconInitial;
+    };
+  }
+  toggleSidebar(){
+    this.isSidebarOpen = !this.isSidebarOpen
   }
 }
-}
+
+
+
